8e7e63a83e835e4a2ebfd760444107b66df52ac1
miniwolf <miniwolf1508@gmail.com>
2017-10-23 15:43:39 +0200

    Fix problem where declaring type was null in calculating the inheritance chain length.

--- Source/Weavers/Unity.Cecil.Visitor/TypeReferenceExtensions.cs
+++ Source/Weavers/Unity.Cecil.Visitor/TypeReferenceExtensions.cs
@@ -6,6 +6,8 @@ namespace Unity.Cecil.Visitor
     {
         public static int InheritanceChainLength(TypeReference type)
         {
+            if (type.DeclaringType == null)
+                return 0;
             var baseType = type.Resolve().BaseType;
             if (baseType == null)
                 return 1;
